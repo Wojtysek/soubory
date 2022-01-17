c8eeac2c2ca21885e8fb2b36217b6cc561a5b36b
Vojtech Hruby <vojtech.hruby.020@pslib.cz>
2022-01-17 13:11:25 +0100

    Přidejte soubory projektu.

---
+++ soubory/soubory.cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	FILE* soubor = fopen("pokus.txt", "w");
+	if (soubor == NULL) {
+		printf("chyba: Soubor nelze otevrit pro zapis.\n");
+		return 1;
+	}
+}
